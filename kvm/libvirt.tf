58c1c2d74dc3ebd380b287a1d0737d3b22d9fd96
rubbercable <rubbercable@office-localdomain.home>
2019-11-07 01:31:05 +0000

    initial kvm

---
+++ kvm/libvirt.tf
@@ -0,0 +1,44 @@
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+#provider "libvirt" {
+#  alias = "server2"
+#  uri   = "qemu+ssh://[email protected]/system"
+#}
+
+resource "libvirt_volume" "centos7-qcow2" {
+  name = "centos7.qcow2"
+  pool = "gnome-boxes"
+  #source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  #source = "./CentOS-7-x86_64-GenericCloud.qcow2"
+  source = "/home/rubbercable/code/terraform/CentOS-7-x86_64-GenericCloud.qcow2"
+  format = "qcow2"
+}
+
+# Define KVM domain to create
+resource "libvirt_domain" "db1" {
+  name   = "db1"
+  memory = "1024"
+  vcpu   = 1
+
+  network_interface {
+    network_name = "default"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.centos7-qcow2.id}"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
